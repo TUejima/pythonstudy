2c594b9fc9e972816d5c481a8ade6e54f0826018
TUejima <bhtu0128@gmail.com>
2019-06-20 12:47:06 +0900

    game update

--- pygame01/game.py
+++ pygame01/game.py
@@ -103,3 +103,190 @@ class Straight_Rope(Rope):
         elif(self.direction == "LEFT"):
             self.x -= self.velocity
         pygame.draw.line(screen, COLORS[3], [self.x, 0], [self.x, 480], 5)
+
+        # checks if the player and the rope cllided
+        def judge(self, octo_cat):
+            if(self.x > (octo_cat.x) and self.x < (octo_cat + 20)):
+                return True
+            else:
+                return False
+
+
+# horizontal ropes
+class Straight_Rope_Horizontal(Rope):
+    def update(self):
+        if(self.y > 475):
+            self.direction = "DOWN"
+        elif(self.y < 5):
+            self.direction = "UP"
+        if(self.direction == "DOWN"):
+            self.y -= self.velocity
+        elif(self.direction == "UP"):
+            self.y += self.velocity
+        pygame.draw.line(screen, COLORS[3], [0, self.y], [640, self.y], 5)
+
+        # checks if the player and the rope collided
+        def judge(self, octo_cat):
+            if(self.y > (octo_cat.y) and self.y < (octo_cat.y + 20)):
+                return True
+            else:
+                return False
+
+
+# dots
+class Shooting_Star(Rope):
+    def update(self):
+        self.x += self.tilt
+        self.y += self.velocity
+        pygame.draw.circle(screen, COLORS[self.x, self.y], 6)
+
+    # checks if the player and the dot collided
+    def judge(self, octo_cat):
+        if((self.y > (octo_cat.y) and self.y < (octo_cat.y + 20)) and (
+                self.x > (octo_cat.x) and self.x < (octo_cat.x + 20))):
+            return True
+        else:
+            return False
+
+
+def open():
+    endFlag = False
+    font1 = pygame.font.SysFont(None, 80)
+    text1 = font1.render("Jump the Rope", False, (255, 255, 255))
+    font2 = pygame.font.SysFont(None, 40)
+    text2 = font1.render("Press any Key to Start", False, (255, 255, 255))
+
+    while endFlag == False:
+        screen.fill((0, 0, 0))
+        screen.blit(text1, (30, 50))
+        screen.blit(text1, (30, 50))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                endFlag - True
+            elif event.type == pygame.KEYDOWN:
+                endFlag = True
+                main()
+
+
+def main():
+    endFlag = False
+    octo_cat = Octo_Cat(400, 400)
+    time_elapsed = 0
+    force_quit = False
+
+    ropes = []
+
+    while endFlag == False:
+        clock.tick(60)
+        time_elapsed += 1
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                endFlag = True
+                force_quit = True
+            else:
+                octo_cat.update(event)
+
+
+# move the player
+if octo_cat.move_right == True:
+    if octo_cat.x < 620:
+        octo_cat.x += OCTO_CAT_VELOCITY
+    if octo_cat.move_left == True:
+        if octo_cat.x > 00:
+            octo_cat.x -= OCTO_CAT_VELOCITY
+    if octo_cat.move_up == True:
+        if octo_cat.y > 00:
+            octo_cat.y -=OCTO_CAT_VELOCITY
+    if octo_cat.move_down == True:
+        if octo_cat.y < 460:
+            octo_cat.y  -= OCTO_CAT_VELOCITY
+
+
+# make the instrances of ropes and dots
+if (time_elapsed == 20):
+            straight_rope = Straight_Rope(0,0,3)
+            ropes.append(straight_rope)
+        if (time_elapsed == 300):
+            straight_rope_horizontal = Straight_Rope_Horizontal(0,0,3)
+            ropes.append(straight_rope_horizontal)
+        if (time_elapsed == 600):
+            straight_rope = Straight_Rope(0,0,3)
+            ropes.append(straight_rope)
+        if (time_elapsed == 860):
+            straight_rope_horizontal = Straight_Rope_Horizontal(0,0,3)
+            ropes.append(straight_rope_horizontal)
+        if(random.randrange(200) < 6):
+            shooting_star1 = Shooting_Star(random.randrange(640),0,random.randrange(5) + 5,random.randrange(10) - 5)
+            ropes.append(shooting_star1)
+            shooting_star2 = Shooting_Star(random.randrange(640),0,random.randrange(5) + 5,random.randrange(10) - 5)
+            ropes.append(shooting_star2)
+            shooting_star3 = Shooting_Star(10,random.randrange(480),random.randrange(5) + 5,random.randrange(10) - 5)
+            ropes.append(shooting_star3)
+            shooting_star4 = Shooting_Star(10,random.randrange(480),random.randrange(5) + 5,random.randrange(10) - 5)
+            ropes.append(shooting_star4)
+
+        #move all the ropes and dots
+        for rope in ropes:
+            rope.update()
+            if (rope.x < 0 or rope.x > 640) or (rope.y < 0 or rope.y > 480):
+                ropes.remove(rope)
+            if(time_elapsed % 1000 == 0) and time_elapsed != 0:
+                rope.velocity += 1
+
+        #if the player is jumping, do not check if it collided with ropes or dots
+        if(octo_cat.immunity == True):
+            octo_cat.immunity_count += 1
+            if (octo_cat.immunity_count < OCTO_CAT_JUMP ):
+                screen.blit(octo_cat.immune_image,(octo_cat.x,octo_cat.y))
+            else:
+                octo_cat.immunity = False
+                octo_cat.immunity_count = 0
+                screen.blit(octo_cat.image,(octo_cat.x,octo_cat.y))
+        else:
+            screen.blit(octo_cat.image,(octo_cat.x,octo_cat.y))
+            for rope in ropes:
+                if(rope.judge(octo_cat) == True) and (octo_cat.life_lost_time + 30 < time_elapsed):
+                    octo_cat.life_lost_time = time_elapsed
+                    octo_cat.life -= 1
+                    if octo_cat.life == 0:
+                        endFlag = True
+        for i in range(octo_cat.life - 1):
+            screen.blit(heart_image,(i * 30,50))
+        pygame.display.update()
+    quit(time_elapsed,force_quit)
+
+#when quitting the game
+def quit(score,force_quit):
+    if force_quit == False:
+        endFlag = False
+        yourScore = "your score: " + str(score)
+        font1 = pygame.font.SysFont(None, 40)
+        text1 = font1.render(yourScore, False, (255,255,255))
+        font2 = pygame.font.SysFont(None, 40)
+        text2 = font1.render("Press Any Key to Re-Start", False, (255,255,255))
+
+        while endFlag == False:
+            screen.fill((0,0,0))
+            screen.blit(text1,(20,50))
+            screen.blit(text2,(20,150))
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    endFlag = True
+                elif event.type == pygame.KEYDOWN:
+                    endFlag = True
+                    main()
+    #retrieve the highest score
+    data = np.loadtxt("score/score.tsv",dtype="str",delimiter=",")
+    highest_score = data[1]
+    print("The highest score so far: " + highest_score)
+    if(score > int(highest_score)):
+        save_data = np.array([str(datetime.datetime.today()),str(score)])
+        np.savetxt('score/score.tsv',save_data,delimiter=',', fmt="%s")
+    pygame.quit()
+
+if __name__ == "__main__":
+    open()
